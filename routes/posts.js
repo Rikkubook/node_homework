68ffffeb35cb05f629a99d75d3e050b64cc6fdaa
林佩瑤 <rikkubook@gmail.com.tw>
2022-05-05 15:38:22 +0800

    添加 新增/移除 同時修改文章

--- routes/posts.js
+++ routes/posts.js
@@ -6,22 +6,19 @@ const { successHandler, errorHandler } = require('../handler');
 
 
 router.get('/', async (req, res) => {
-  // http://localhost:3005/posts?timeSort=asc&search=0
+  // http://localhost:3005/posts?timeSort=asc&search=
   const timeSort = req.query.timeSort == 'asc'? 1:-1
-  // console.log(req.query.search == false)
-  const search = req.query.search? {"content": new RegExp(req.query.search)} : {}; //== undefined??? 空字串
+  const search = req.query.search? {"content": new RegExp(req.query.search)} : {}; 
   try{
     const posts =await Post.find(search).populate({ //此是先將name編譯,可以不編譯只算長度
       path: 'userInfo',
       select: 'name photo'
-    }).sort({'createAt': timeSort})  //1 由小到大
+    }).sort({'createAt': timeSort})  //.sort(timeSort)
     successHandler(res, posts)
   }catch(error){
     errorHandler(res,error,400)
   }
 });
-
-//https://mongoosejs.com/docs/populate.html#populate_multiple_documents
 
 router.post('/', async (req, res) => {
   try{
@@ -54,29 +51,49 @@ router.delete('/', async (req, res) => {
 
 
 //----------------------------------------------------------------
+
+///???? 如果刪除文章時 收藏這邊要刪嗎?? 還是回傳此文章已刪就好
+
+//新增-移除
 router.post('/postLikes', async (req, res) => {
   try{
     const data = req.body
     if(!data.userInfo || !data.posts ){
-      throw '使用者Id、喜歡文章、缺一不可'
+      throw '使用者Id、喜歡文章Id、缺一不可'
     }
 
     const user = await Like.findOne({"userInfo": data.userInfo})
+    const post = await Post.findOne({"_id": data.posts})
+
     if(user){
-      if(user.posts.includes(data.posts)){ //為了修改排序
-        const index = like.posts.indexOf(data.posts)
-        console.log(index)
-        user.posts.splice(index, 1) 
+      if(user.posts.includes(data.posts)){ //移除
+        const postsIndex = user.posts.indexOf(data.posts)
+        user.posts.splice(postsIndex, 1) 
+        user.save()
+
+        // 移除時文章連動
+        const likesIndex = post.likes.indexOf(data.userInfo)
+        post.likes.splice(likesIndex, 1) 
+        post.save()
+
+        successHandler(res, user)
+      }else{ //收藏
+        user.posts.unshift(data.posts) 
+        user.save()
+
+        // 收藏時文章連動
+        post.likes.unshift(data.userInfo) 
+        post.save()
+        successHandler(res, user)
       }
-      user.posts.unshift(data.posts) 
-      user.save()
-      successHandler(res, user)
-      // https://mongoosejs.com/docs/api/array.html#mongoosearray_MongooseArray-addToSet
     }else{
       const newLike = await Like.create({
         userInfo: data.userInfo,
         posts: [data.posts],
       })
+       // 收藏時文章連動
+      post.likes.unshift(data.userInfo) 
+      post.save()
       successHandler(res, newLike)
     }
   }catch(error){
@@ -84,11 +101,12 @@ router.post('/postLikes', async (req, res) => {
   }
 });
 
+// 查詢
 router.post('/likes', async (req, res) => {
   try{
     const data = req.body
     if(!data.userInfo ){
-      throw '名稱缺少'
+      throw '使用者Id缺一不可'
     }
 
     const likes = await Like.findOne({"userInfo": data.userInfo})
@@ -101,6 +119,15 @@ router.post('/likes', async (req, res) => {
     })
 
     successHandler(res, likes)
+  }catch(error){
+    errorHandler(res,error,400)
+  }
+});
+
+router.delete('/likes', async (req, res) => {
+  try{
+    await Like.deleteMany({});
+    successHandler(res, [])
   }catch(error){
     errorHandler(res,error,400)
   }
