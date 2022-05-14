8c11705eea72f5a8a797127779cc8d5251765d07
Rikku <rikkubook@gmail.com>
2022-05-14 16:49:59 +0800

    分開 開發/產品錯誤

--- app.js
+++ app.js
@@ -52,14 +52,48 @@ app.use(function(req,res,next){
   })
 })
 
+// express 錯誤處理
+// 正式環境錯誤
+const resErrorProd = (err, res) => {
+  if (err.isOperational) { //可預期
+    res.status(err.statusCode).json({
+      message: err.message
+    });
+  } else {
+    // log 紀錄
+    console.error('出現重大錯誤', err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: 'error',
+      message: '系統錯誤，請恰系統管理員'
+    });
+  }
+};
+// 開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
 // express 錯誤處理 //next
-app.use(function(err,req,res,next){
-  console.log('3.express 錯誤處理')
-  res.status(err.statusCode || 500).json({
-      "message": err.message,
-      "statusCode": err.statusCode
-  })
-})
+app.use(function(err, req, res, next) {
+  // dev
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  } 
+  // production
+  if (err.name === 'ValidationError'){ //mongoose 的欄位錯誤 error.name
+    err.message = "資料欄位未填寫正確，請重新輸入！"
+    err.isOperational = true;
+    return resErrorProd(err, res)
+  }
+  resErrorProd(err, res)
+});
+
 
 // 未捕捉到的 catch 
 process.on('unhandledRejection', (err, promise) => {
